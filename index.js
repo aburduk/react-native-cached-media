3e95d30487c1562fa66814ceac8bd33a33ad6c67
Artem Burduk <aburduk@ngoar.net>
2019-02-04 17:28:48 +0200

    add video mp4 support
    add video mp4 support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports.CachedImage = require('./CachedImage');
+module.exports.CachedVideo = require('./CachedVideo');
 module.exports.ImageCacheProvider = require('./ImageCacheProvider');
 module.exports.ImageCacheManager = require('./ImageCacheManager');
 module.exports.ImageCachePreloader = require('./ImageCachePreloader');
